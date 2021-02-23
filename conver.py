eabe86cd2220dba34c64a6d988bd58bfd260ef96
Kavya Prasad <kavpras@i7.red.uits.iu.edu>
2021-02-22 22:48:05 -0500

    First commit

---
+++ conver.py
@@ -0,0 +1,23 @@
+#import tensorflow as tf
+
+# Convert the model
+
+#converter = tf.lite.TFLiteConverter.from_saved_model('rev2') 
+#tflite_model = converter.convert()
+
+# Save the model.
+#with open('rev2.tflite', 'wb') as f:
+  #f.write(tflite_model)
+  
+import tensorflow as tf
+
+converter = tf.lite.TFLiteConverter.from_keras_model('arjuna')
+converter.target_spec.supported_ops = [
+  tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
+  tf.lite.OpsSet.SELECT_TF_OPS, # enable TensorFlow ops.
+  tf.lite.OpsSet.TFLITE_BUILTINS_INT8
+]
+tflite_model = converter.convert()
+with open('arjuna.tflite', 'wb') as f:
+  f.write(tflite_model)
+
